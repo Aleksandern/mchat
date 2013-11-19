484125deb81502769bb34f2224ff99113b371a04
aleksandern <aleksandern08@gmail.com>
2013-11-19 08:29:52 +0700

    merge commints

--- index.php
+++ index.php
@@ -1,9 +1,4 @@
 <?php
-//error_reporting (E_ALL);
-
-//define ('DIRSEP', DIRECTORY_SEPARATOR); 
-//define ('site_path', dirname(__FILE__));
-//define ('app', 'app');
 
 require_once 'app'.DIRECTORY_SEPARATOR.'Bootstrap.php';
 
